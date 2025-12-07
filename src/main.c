692cba328fabd0e16f4662b619a79f3e851c2ec0
Raymond Wong <raymondwong1701@gmail.com>
2025-12-06 21:11:47 -0800

    Improved tap callback

--- src/main.c
+++ src/main.c
@@ -53,9 +53,12 @@
 #define SPEED_X1 250
 #define SPEED_Y1 3
 #define SPEED_X2 296
-#define SPEED_Y2 15
+#define SPEED_Y2 14
 
 #define MAX_APS 50
+#define TAP_RADIUS 10
+
+#define TEXT_SPACING 24
 
 typedef struct {
     char ssid[32];
@@ -63,11 +66,21 @@ typedef struct {
     uint8_t rate;
     uint8_t sig_mode;
     uint8_t channel;
-	uint8_t bandwidth;
+    uint8_t bandwidth;
     uint32_t timestamp;
+    bool visible;
+    uint16_t x;
+    uint16_t y;
 } ap_info_t;
 
 QueueHandle_t ap_queue;
+
+typedef struct {
+    uint16_t x;
+    uint16_t y;
+} touch_event_t;
+
+QueueHandle_t touch_queue;
 
 const static char *TAG = "wifi_mapper";
 
@@ -119,6 +132,31 @@ void draw_channel_selection(uint8_t channel, uint16_t color) {
     }
 }
 
+
+void draw_x_values(bool useFrequencies) {
+    char text[32];
+
+    LCD_DrawFillRectangle(PAUSE_X2 + 5, BASELINE + 5, SCREEN_W, SCREEN_H, BLACK);
+
+    LCD_Set_Orientation(LCD_DISPLAY_ORIENTATION_PORTRAIT_INVERTED); // to print text sideways
+    for (uint16_t i = 1; i <= CHANNEL_COUNT; i++) {
+        if (useFrequencies) {
+            sprintf(text, "%d", i * CHANNEL2FREQ);
+        } else {
+            sprintf(text, "Ch.%d", i);
+        }
+
+        LCD_ShowString(BASELINE + 7, SCREEN_W - (i * CHANNEL2X) - 7, BLACK, WHITE, 12, text, 0);
+    }
+    LCD_Set_Orientation(LCD_DISPLAY_ORIENTATION_LANDSCAPE);
+}
+
+
+void draw_info(ap_info_t info) {
+    LCD_DrawFillRectangle(PAUSE_X2 + 5, BASELINE + 7, SCREEN_W - 5, SCREEN_H - 5, GRAY);
+}
+
+
 bool isBetween(uint16_t value, uint16_t min, uint16_t max) {
     if (value > max) {
         return false;
@@ -127,6 +165,18 @@ bool isBetween(uint16_t value, uint16_t min, uint16_t max) {
         return false;
     }
     return true;
+}
+
+
+// not actual distance
+uint16_t crude_distance(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
+    uint16_t min_x = (x1 < x2) ? x1 : x2;
+    uint16_t max_x = (x1 > x2) ? x1 : x2;
+    uint16_t d_x = max_x - min_x;
+    uint16_t min_y = (y1 < y2) ? y1 : y2;
+    uint16_t max_y = (y1 > y2) ? y1 : y2;
+    uint16_t d_y = max_y - min_y;
+    return (d_x > d_y) ? d_x : d_y;
 }
 
 
@@ -162,6 +212,7 @@ void sniffer_task(void *pvParameters) {
     ap_info_t sync;
     sync.rssi = 0;
     sync.bandwidth = 0;
+    sync.visible = false;
 
     while (1) {
         if (playing) {
@@ -172,8 +223,11 @@ void sniffer_task(void *pvParameters) {
 		};
         sync.channel = channel;
         
-        xQueueSend(ap_queue, &sync, NULL);
-        esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+        if (playing) {
+            xQueueSend(ap_queue, &sync, NULL);
+            esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+        }
+
         vTaskDelay(pdMS_TO_TICKS(speed_setting * SPEED2INTERVAL));
     }
 }
@@ -181,6 +235,12 @@ void sniffer_task(void *pvParameters) {
 
 void render_task(void *pvParameters) {
     ap_info_t info;
+    touch_event_t event;
+    ap_info_t packetCache[MAX_APS];
+    uint8_t cacheIndex = 0;
+    int8_t selectedIndex = -1;
+
+    bool usingFreqLabel = false;
 
     LCD_DrawFillRectangle(0, 0, SCREEN_W, SCREEN_H, BLACK); // Clear screen
 
@@ -191,13 +251,9 @@ void render_task(void *pvParameters) {
         line_x = i * CHANNEL2X;
         LCD_DrawLine(line_x, -10 * RSSI2Y, line_x, BASELINE, GRAY);
         LCD_DrawLine(line_x, BASELINE, line_x, BASELINE + 4, WHITE);
-
-        sprintf(text, "Ch.%d", i);
-
-	    LCD_Set_Orientation(LCD_DISPLAY_ORIENTATION_PORTRAIT_INVERTED);
-        LCD_ShowString(BASELINE + 7, SCREEN_W - line_x - 7, BLACK, WHITE, 12, text, 0);
-	    LCD_Set_Orientation(LCD_DISPLAY_ORIENTATION_LANDSCAPE);
     }
+
+    draw_x_values(usingFreqLabel);
 
     // horizontal grid lines (rssi)
     uint16_t line_y = 0;
@@ -212,28 +268,79 @@ void render_task(void *pvParameters) {
     LCD_DrawLine(CHANNEL2X, BASELINE, SCREEN_W, BASELINE, WHITE);
 
     uint16_t delayInTicks = pdMS_TO_TICKS(speed_setting * SPEED2INTERVAL - 1);
-    bool wasPlaying = false;
-    uint8_t prevSpeed = 0;
+
+    // draw initial values for buttons
+    draw_pause();
+    sprintf(text, "SPEED: %d", MIN_SPEED);
+    LCD_DrawFillRectangle(SPEED_X1 - 2, SPEED_Y1, SPEED_X2 + 2, SPEED_Y2, GRAY);
+    LCD_ShowString(SPEED_X1, SPEED_Y1, GRAY, WHITE, 12, text, 1);
 
     while (1) {
-        // check if speed_setting was changed
-        if (speed_setting != prevSpeed) {
-            sprintf(text, "SPEED: %d", speed_setting);
-            LCD_DrawFillRectangle(SPEED_X1, SPEED_Y1, SPEED_X2, SPEED_Y2, GRAY);
-            LCD_ShowString(SPEED_X1, SPEED_Y1, GRAY, WHITE, 12, text, 1);
-            delayInTicks = pdMS_TO_TICKS(speed_setting * SPEED2INTERVAL - 1);
-            prevSpeed = speed_setting;
-            vTaskDelay(delayInTicks);
-        }
-        // check if pause/play was pressed
-        if (playing ^ wasPlaying) {
-            if (playing) {
-                draw_pause();
-            } else {
-                draw_play();
+        if (xQueueReceive(touch_queue, &event, 0)) {
+            bool eventHandled = false;
+
+            // clear circle around selected packet, if any
+            if (selectedIndex > 0) {
+                LCD_Draw_Circle(packetCache[selectedIndex].x, packetCache[selectedIndex].y, 3, BLACK);
+                draw_x_values(usingFreqLabel);
+                selectedIndex = -1;
             }
-            wasPlaying = playing;
-            vTaskDelay(delayInTicks);
+
+            // pause/play button
+            if (!eventHandled && isBetween(event.x, PAUSE_X1, PAUSE_X2) && isBetween(event.y, PAUSE_Y1, PAUSE_Y2)) {
+                playing = !playing;
+
+                if (playing) {
+                    draw_pause();
+                } else {
+                    draw_play();
+                }
+                
+                ESP_LOGI(TAG, "Playing: %d", playing);
+                eventHandled = true;
+            }
+            
+            // speed
+            if (!eventHandled && event.x > SPEED_X1 && event.y < SPEED_Y2) {
+                speed_setting++;
+                if (speed_setting > MAX_SPEED) {
+                    speed_setting = MIN_SPEED;
+                }
+
+                sprintf(text, "SPEED: %d", speed_setting);
+                LCD_DrawFillRectangle(SPEED_X1, SPEED_Y1, SPEED_X2, SPEED_Y2, GRAY);
+                LCD_ShowString(SPEED_X1, SPEED_Y1, GRAY, WHITE, 12, text, 1);
+                delayInTicks = pdMS_TO_TICKS(speed_setting * SPEED2INTERVAL - 1);
+                
+                ESP_LOGI(TAG, "Speed: %d", speed_setting);
+                eventHandled = true;
+            }
+
+            // check if x-axis was tapped on
+            if (!eventHandled && event.y > BASELINE) {
+                usingFreqLabel = !usingFreqLabel;
+                draw_x_values(usingFreqLabel);
+                eventHandled = true;
+            }
+
+            // check if tap was on a graphed packet
+            if (!eventHandled) {
+                for (uint16_t i = 0; i < MAX_APS; i++) {
+                    if (packetCache[i].visible && crude_distance(packetCache[i].x, packetCache[i].y, event.x, event.y) < TAP_RADIUS) {
+                        // pause scanning
+                        playing = false;
+                        draw_play();
+
+                        selectedIndex = i;
+                        draw_info(packetCache[i]);
+
+                        // circle the packet
+                        LCD_Draw_Circle(packetCache[i].x, packetCache[i].y, 3, WHITE);
+
+                        break;
+                    }
+                }
+            }
         }
         if (playing && xQueueReceive(ap_queue, &info, delayInTicks)) {
             if (info.rssi == 0 && info.bandwidth == 0) { // sync frame
@@ -243,11 +350,32 @@ void render_task(void *pvParameters) {
                 } else {
                     draw_channel_selection(info.channel + 1, GREEN);
                 }
-            } else {
-                ESP_LOGI(TAG, "Freq: %d\tBandwidth: %d\tRSSI: %ddB", info.channel * CHANNEL2FREQ, info.bandwidth, info.rssi);
 
-                LCD_Draw_FillCircle(info.channel * CHANNEL2X, info.rssi * RSSI2Y, 1, GREEN);
+                // loop through all cached packets and mark old ones as "invisible"
+                for (uint16_t i = 0; i < MAX_APS; i++) {
+                    if (packetCache[i].channel == info.channel) {
+                        packetCache[i].visible = false;
+                    }
+                }
+
+            } else {
+                ESP_LOGI(TAG, "Freq: %d\tBandwidth: %d\tRSSI: %ddBm", info.channel * CHANNEL2FREQ, info.bandwidth, info.rssi);
+                
+                ap_info_t copy = info;
+                copy.x = info.channel * CHANNEL2X;
+                copy.y = info.rssi * RSSI2Y;
+                copy.visible = true;
+
+                packetCache[cacheIndex] = copy;
+                cacheIndex++;
+                if (cacheIndex >= MAX_APS) {
+                    cacheIndex = 0;
+                }
+
+                LCD_Draw_FillCircle(copy.x, copy.y, 1, GREEN);
             }
+        } else if (!playing) {
+            vTaskDelay(delayInTicks);
         }
         vTaskDelay(pdMS_TO_TICKS(1));
     }
@@ -276,6 +404,9 @@ void app_main(void) {
     ESP_LOGI(TAG, "Creating AP queue...");
     ap_queue = xQueueCreate(MAX_APS, sizeof(ap_info_t));
 
+    ESP_LOGI(TAG, "Creating touch event queue...");
+    touch_queue = xQueueCreate(10, sizeof(touch_event_t));
+
 	ESP_LOGI(TAG, "Clearing screen...");
 	Init_LCD(WHITE);
 
@@ -291,27 +422,21 @@ void app_main(void) {
     xTaskCreate(sniffer_task, "sniffer_task", 4096, NULL, 5, NULL);
 
 	ESP_LOGI(TAG, "Starting render task...");
-    xTaskCreate(render_task, "render_task", 4096, NULL, 2, NULL);
+    xTaskCreate(render_task, "render_task", 8192, NULL, 2, NULL);
 
     bool touch_read = false;
     bool posedge = false;
 
+    touch_event_t event = {
+        .x = TouchX,
+        .y = TouchY
+    };
     while (1) { // touch event loop
         touch_read = xpt2046_read();
 		if (touch_read && !posedge) {
-            // pause/play button
-            if (isBetween(TouchX, PAUSE_X1, PAUSE_X2) && isBetween(TouchY, PAUSE_Y1, PAUSE_Y2)) {
-                playing = !playing;
-                ESP_LOGI(TAG, "Playing: %d", playing);
-            }
-            // speed
-            if (TouchX > SPEED_X1 && TouchY < SPEED_Y2) {
-                speed_setting++;
-                if (speed_setting > MAX_SPEED) {
-                    speed_setting = MIN_SPEED;
-                }
-                ESP_LOGI(TAG, "Speed: %d", speed_setting);
-            }
+            event.x = TouchX;
+            event.y = TouchY;
+            xQueueSend(touch_queue, &event, 0);
 		}
         posedge = touch_read;
 		vTaskDelay(50 / portTICK_PERIOD_MS);
